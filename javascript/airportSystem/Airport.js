c46dac16c4bfc8ce06bd74afd5c64cd6d1f2c072
Rafael Barros Parigi <rafaelparigi@rafaels-air-2.home>
2021-10-15 11:22:17 +0100

    my second commit

--- javascript/airportSystem/Airport.js
+++ javascript/airportSystem/Airport.js
@@ -1,8 +1,11 @@
 class Airport {
+    static airports = [];
+
     constructor(name) {
         this.name = name
         if (name === undefined)
             throw new Error('airport must have a name');
+        Airport.airports.push(this);
     }
 };
 
--- javascript/airportSystem/Airport.test.js
+++ javascript/airportSystem/Airport.test.js
@@ -5,4 +5,9 @@ describe('Airport', () => {
     test('name is', () => {
         expect(() => new Airport()).toThrowError('airport must have a name');
     })
+    test('list of airports:', () => {
+        const london = new Airport('LHX');
+        const paris = new Airport('PRS');
+        expect(Airport.airports.length).toBe(2);
+    })
 });
