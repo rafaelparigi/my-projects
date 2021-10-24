a839723c8b04330bd63a229dfcaca0e265adf68a
Rafael Barros Parigi <rafaelparigi@gmail.com>
2021-10-24 21:39:16 +0100

    added comment about async await

--- javascript/scooterSystem/ChargingStation.js
+++ javascript/scooterSystem/ChargingStation.js
@@ -7,6 +7,9 @@ class ChargingStation {
     async chargeScooter(scooter) {
         console.log('Starting charge');
         //set scooter to available and charged after a certain amount of time 0 ~ 2hours
+        /*If you make a function async then, when you run that function in the code, it does not block the code below it. 
+        Inside the function, the await keyword tells your code to wait for a value. 
+        The reason you need this is because sometimes code doesn't automatically give you the result you need, so you need to await the result via a Promise.*/
         await new Promise((resolve, reject) => {
             setTimeout(() => {
                 scooter.chargingLevel = 1;
@@ -19,4 +22,3 @@ class ChargingStation {
 };
 
 module.exports = { ChargingStation };
-
